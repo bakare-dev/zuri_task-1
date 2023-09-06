898f8bbf14b90c5de3b73fdb6fa71290116a72d6
112479953+bakare-dev@users.noreply.github.com <112479953+bakare-dev@users.noreply.github.com>
2023-09-06 22:19:21 +0100

    commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # zuri_task-1
 
 
-url = https://bakare-zuir-task-1.onrender.com/?slack_name=bakare&track=backend
+url = https://bakare-zuir-task-1.onrender.com/api?slack_name=bakare&track=backend
--- server/routes/route.js
+++ server/routes/route.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const Controller = require("../../controller/Controller");
 const controller = new Controller();
 
-router.get("/", controller.getdata);
+router.get("/api", controller.getdata);
 
 module.exports = router;
